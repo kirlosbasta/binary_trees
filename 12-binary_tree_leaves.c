e19c8b20abb6246c2a61ee794f057401e7384e1c
kirlos basta <kirlosbasta@gmail.com>
2024-03-01 04:21:30 +0200

    Add 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Find number of leaves in a tree
+ * @tree: Node in a tree
+ *
+ * Return: The number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+}
