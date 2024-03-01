7b7490338ede8009f2c3375098a20653cde891ea
kirlos basta <kirlosbasta@gmail.com>
2024-03-02 00:11:36 +0200

    Add 14-binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,6 +10,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
 
+	if (!tree)
+	{
+		return (0);
+	}
 	l = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	r = tree->right ? (int)binary_tree_height(tree->right) : -1;
 	return (l - r);
