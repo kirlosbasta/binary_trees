14371143fe3b904070a4bb5c38c4d3187b7becbf
kirlos basta <kirlosbasta@gmail.com>
2024-03-01 04:33:14 +0200

    Add 13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Find number of nodes in a tree
+ * @tree: Node in a tree
+ *
+ * Return: The number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+	return 1 + (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
