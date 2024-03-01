18877cd96a3aaa5204fe4ba01957e7fcadf2fdf4
kirlos basta <kirlosbasta@gmail.com>
2024-03-02 01:46:47 +0200

    Add 15-binary_tree_is_full

--- main.c
+++ main.c
@@ -9,13 +9,22 @@
  */
 int main(void)
 {
-	binary_tree_t *root;
-	int balance;
+    binary_tree_t *root;
+    int full;
 
-	root = NULL;
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root, 128);
+    root->left->left = binary_tree_node(root->left, 10);
+    binary_tree_print(root);
 
-	balance = binary_tree_balance(root);
-	printf("Balance factor for tree with root node (%p): %+d\n", (void *)root, balance);
-
-	return (0);
+    full = binary_tree_is_full(root);
+    printf("Is %d full: %d\n", root->n, full);
+    full = binary_tree_is_full(root->left);
+    printf("Is %d full: %d\n", root->left->n, full);
+    full = binary_tree_is_full(root->right);
+    printf("Is %d full: %d\n", root->right->n, full);
+    return (0);
 }
